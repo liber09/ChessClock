a0a106d4ad56627835ef9110e563ad0a363499f8
liber09 <113246195+liber09@users.noreply.github.com>
2023-03-21 08:28:44 +0100

    Initial Commit

---
+++ ChessClock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessClock
+//
+//  Created by Linda Bergsängel on 2023-03-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
